e1625ec0c801c855e94f6d6c2115121003650744
Dalton <dsteele92@gmail.com>
2023-01-14 23:06:42 -0800

    reset with new react app to remove unneeded dependencies

--- src/components/Booking/BookingWeb.jsx
+++ src/components/Booking/BookingWeb.jsx
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import Style from './bookingWeb.module.scss';
 import { TfiArrowCircleRight } from 'react-icons/tfi';
 
 export default function BookingWeb() {
+	const [startDate, setStartDate] = useState(null);
+	const [endDate, setEndDate] = useState(null);
+	const [focusedInput, setFocusedInput] = useState(null);
+
 	return (
 		<div className={Style.Book}>
-			<div className={Style.Booker}>
-				<div>Check In</div>
-				<div>Check Out</div>
+			{/* <div className={Style.Booker}>
+				<DateRangePicker
+					startDate={startDate} // momentPropTypes.momentObj or null,
+					startDateId='your_unique_start_date_id' // PropTypes.string.isRequired,
+					endDate={endDate} // momentPropTypes.momentObj or null,
+					endDateId='your_unique_end_date_id' // PropTypes.string.isRequired,
+					onDatesChange={({ startDate, endDate }) => {
+						setStartDate(startDate);
+						setEndDate(endDate);
+					}} // PropTypes.func.isRequired,
+					focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+					onFocusChange={(focusedInput) => setFocusedInput(focusedInput)} // PropTypes.func.isRequired,
+				/>
 				<div>Guests</div>
 			</div>
 			<div className={Style.Button}>
@@ -16,7 +30,7 @@ export default function BookingWeb() {
 					Availability
 				</h4>
 				<TfiArrowCircleRight />
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- src/components/GoogleMaps/GoogleMaps.jsx
+++ src/components/GoogleMaps/GoogleMaps.jsx
@@ -1,16 +1,16 @@
-import React, { useMemo } from 'react';
-import Style from './googleMaps.module.scss';
-import { GoogleMap, useLoadScript, Marker, Circle } from '@react-google-maps/api';
+// import React, { useMemo } from 'react';
+// import Style from './googleMaps.module.scss';
+// import { GoogleMap, useLoadScript, Marker, Circle } from '@react-google-maps/api';
 
-export default function GoogleMaps() {
-	const { isLoaded } = useLoadScript({ googleMapsApiKey: '' });
-	const center = useMemo(() => ({ lat: 45.51, lng: -122.603 }));
+// export default function GoogleMaps() {
+// 	const { isLoaded } = useLoadScript({ googleMapsApiKey: '' });
+// 	const center = useMemo(() => ({ lat: 45.51, lng: -122.603 }));
 
-	if (!isLoaded) return <div className={Style.Loading}>Loading Maps...</div>;
+// 	if (!isLoaded) return <div className={Style.Loading}>Loading Maps...</div>;
 
-	return (
-		<GoogleMap zoom={14} center={center} mapContainerClassName={Style.Maps}>
-			<Circle center={center} radius={500} />
-		</GoogleMap>
-	);
-}
+// 	return (
+// 		<GoogleMap zoom={14} center={center} mapContainerClassName={Style.Maps}>
+// 			<Circle center={center} radius={500} />
+// 		</GoogleMap>
+// 	);
+// }
